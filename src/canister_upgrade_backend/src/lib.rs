8bc6d141902a935605552cfb53b50db3d85f58e3
Stephen-Kimoi <moenisteve@gmail.com>
2024-04-01 12:22:13 +0300

    adding pre upgrade and post upgrade functions for preservation of canisters state

--- src/canister_upgrade_backend/src/lib.rs
+++ src/canister_upgrade_backend/src/lib.rs
@@ -51,4 +51,17 @@ fn add_user(principal: Principal) {
     USERS.with(|users| users.borrow_mut().insert(principal)); 
 }
 
+// This function is called before canister upgrade. The users are saved in the stable storage before a canister upgrade (Thus allowing preservation of the canisters state) 
+#[pre_upgrade] 
+fn pre_upgrade() {
+    USERS.with(|users| storage::stable_save((users,)).unwrap()); 
+}
+
+// This function restores users from stable storage: allowing the state of the canister to be preserved accross upgrades
+#[post_upgrade] 
+fn post_upgrade() {
+    let (old_users,): (BTreeSet<Principal>,) = storage::stable_restore().unwrap(); // This function is called to restore users from the stable storage 
+    USERS.with(|users| *users.borrow_mut() = old_users); 
+}
+
 ic_cdk::export_candid!();
